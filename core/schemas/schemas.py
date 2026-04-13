6730f2e280fbd9c0097841ed511f31667ce5d0cb
Aguilam <pexfex@mail.ru>
2026-04-13 22:08:21 +0400

    feat(core): add base classes for data

--- core/schemas/schemas.py
+++ core/schemas/schemas.py
@@ -1,24 +1,69 @@
-from pydantic import BaseModel
-from enum import StrEnum
+from dataclasses import dataclass
 
 
-class Track(BaseModel):
-    id: str
+@dataclass(slots=True)
+class Track:
+    id: int | None
     title: str
-    artist: list[str]
+    artists_names: list[str] | None
+    artists_id: list[int]
+    album_name: str
+    album_id: int
+    cover_path: str | None
+    path: str
     length: int
+    album_position: int | None
+    bpm: int | None
+    trackGain: float | None
+    trackPeak: float | None
+    discNumber: int | None
+    year: int | None
 
 
-class Album(BaseModel):
-    id: str
+@dataclass(slots=True)
+class Artist:
+    id: int | None
+    name: str
+    cover_path: str | None
 
 
-class Artist(BaseModel):
-    id: str
+@dataclass(slots=True)
+class Album:
+    id: int | None
+    title: str
+    cover_path: str | None
+    duration: int
+    tracks_count: int
+    Artist: Artist
+    tracks: list[Track] | None
 
 
-class QueryType(StrEnum):
-    ALL = "all"
-    TRACK = "track"
-    ALBUM = "album"
-    ARTIST = "artist"
+@dataclass(slots=True)
+class Playlist:
+    id: int | None
+    title: str
+    cover_path: str | None
+    tracks: list[Track] | None
+    public: bool
+    tracks_count: int
+    duration: int
+
+
+@dataclass(slots=True)
+class Lyric:
+    id: int | None
+    is_synced: bool
+    synced_text: dict | None
+    plain_text: str | None
+    language: str
+    path: str
+    type: str
+    offset: int
+    track_id: int | None
+
+
+@dataclass(slots=True)
+class MusicVideo:
+    id: int | None
+    local_link: str
+    track_id: int
