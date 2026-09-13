dbdbadcdc0b4e5bc62f930e9f6c86eab4e2bc5d1
Aguilam <pexfex@mail.ru>
2026-09-13 21:39:38 +0400

    refactor(web ui): move sidebar to widgets

--- interfaces/music_saver/src/App.tsx
+++ interfaces/music_saver/src/App.tsx
@@ -1,29 +1,7 @@
-import { Navigate, useNavigate } from "@solidjs/router";
-import { QueryClient, QueryClientProvider, createQuery } from "@tanstack/solid-query";
-import { Show, type ParentComponent } from "solid-js";
-import {
-  SidebarProvider,
-  SidebarTrigger,
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-  SidebarGroupContent,
-  SidebarMenu,
-  SidebarMenuItem,
-  SidebarMenuButton,
-  SidebarMenuSubItem,
-  SidebarMenuSubButton,
-} from "~/shared/ui/sidebar/sidebar";
-import { user } from "./shared/store/user";
-import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "./shared/ui/collapsible/collapsible";
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { type ParentComponent } from "solid-js";
+import AppSidebar from "./widgets/sidebar/sidebar";
 const App: ParentComponent = (props) => {
-  const navigate = useNavigate();
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -32,94 +10,12 @@ const App: ParentComponent = (props) => {
       },
     },
   });
+  
   return (
     <QueryClientProvider client={queryClient}>
-      <SidebarProvider>
-        <Show when={user.username} fallback={<Navigate href="/auth" />}>
-          <Sidebar>
-            <SidebarHeader />
-            <SidebarContent>
-              <SidebarGroup>
-                <SidebarGroupContent>
-                  <SidebarMenu>
-                    <SidebarMenuItem>
-                      <SidebarMenuButton onClick={() => navigate("/search")}>
-                        Search
-                      </SidebarMenuButton>
-                    </SidebarMenuItem>
-                    <SidebarMenuItem>
-                      <Collapsible>
-                        <SidebarMenuButton>
-                          <CollapsibleTrigger>Content</CollapsibleTrigger>
-                        </SidebarMenuButton>
-                        <CollapsibleContent>
-                          <SidebarMenuSubItem>
-                            <SidebarMenuSubButton onClick={() => navigate("/artists")}>
-                              Artists
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                          <SidebarMenuSubItem>
-                            <SidebarMenuSubButton onClick={() => navigate("/playlists")}>
-                              Playlists
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                          <SidebarMenuSubItem>
-                            <SidebarMenuSubButton onClick={() => navigate("/albums")}>
-                              Albums
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                          <SidebarMenuSubItem>
-                            <SidebarMenuSubButton onClick={() => navigate("/tracks")}>
-                              Tracks
-                            </SidebarMenuSubButton>
-                          </SidebarMenuSubItem>
-                        </CollapsibleContent>
-                      </Collapsible>
-                    </SidebarMenuItem>
-                    <SidebarMenuItem>
-                      <SidebarMenuButton onClick={() => navigate("/")}>Main</SidebarMenuButton>
-                    </SidebarMenuItem>
-                    <Show when={user.isAdmin}>
-                      <SidebarMenuItem>
-                        <SidebarMenuButton onClick={() => navigate("/statuses")}>
-                          Statuses
-                        </SidebarMenuButton>
-                      </SidebarMenuItem>
-                      <SidebarMenuItem>
-                        <SidebarMenuButton onClick={() => navigate("/users")}>
-                          Users
-                        </SidebarMenuButton>
-                      </SidebarMenuItem>
-                    </Show>
-                  </SidebarMenu>
-                </SidebarGroupContent>
-              </SidebarGroup>
-            </SidebarContent>
-            <SidebarFooter>
-              <SidebarGroupContent>
-                <SidebarMenu>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton onClick={() => navigate("/settings")}>
-                      Settings
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                  <SidebarMenuItem>
-                    <SidebarMenuButton onClick={() => navigate(`/profile/${user.id}`)}>
-                      Profile
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                </SidebarMenu>
-              </SidebarGroupContent>
-            </SidebarFooter>
-          </Sidebar>
-        </Show>
-        <main class="w-full h-full">
-          <Show when={user.username}>
-            <SidebarTrigger />
-          </Show>
-          {props.children}
-        </main>
-      </SidebarProvider>
+      <AppSidebar>
+        {props.children}
+      </AppSidebar>
     </QueryClientProvider>
   );
 };
---
+++ interfaces/music_saver/src/widgets/sidebar/sidebar.tsx
@@ -0,0 +1,117 @@
+import { Navigate, useNavigate } from "@solidjs/router";
+import { Show, type ParentComponent } from "solid-js";
+import {
+  SidebarProvider,
+  SidebarTrigger,
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenuSubItem,
+  SidebarMenuSubButton,
+} from "~/shared/ui/sidebar/sidebar";
+import { user } from "~/shared/store/user";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "~/shared/ui/collapsible/collapsible";
+
+const AppSidebar: ParentComponent = (props) => {
+    const navigate = useNavigate();
+
+    return (
+        <SidebarProvider>
+        <Show when={user.username} fallback={<Navigate href="/auth" />}>
+          <Sidebar>
+            <SidebarHeader />
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarGroupContent>
+                  <SidebarMenu>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton onClick={() => navigate("/search")}>
+                        Search
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <Collapsible>
+                        <SidebarMenuButton>
+                          <CollapsibleTrigger>Content</CollapsibleTrigger>
+                        </SidebarMenuButton>
+                        <CollapsibleContent>
+                          <SidebarMenuSubItem>
+                            <SidebarMenuSubButton onClick={() => navigate("/artists")}>
+                              Artists
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                          <SidebarMenuSubItem>
+                            <SidebarMenuSubButton onClick={() => navigate("/playlists")}>
+                              Playlists
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                          <SidebarMenuSubItem>
+                            <SidebarMenuSubButton onClick={() => navigate("/albums")}>
+                              Albums
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                          <SidebarMenuSubItem>
+                            <SidebarMenuSubButton onClick={() => navigate("/tracks")}>
+                              Tracks
+                            </SidebarMenuSubButton>
+                          </SidebarMenuSubItem>
+                        </CollapsibleContent>
+                      </Collapsible>
+                    </SidebarMenuItem>
+                    <SidebarMenuItem>
+                      <SidebarMenuButton onClick={() => navigate("/")}>Main</SidebarMenuButton>
+                    </SidebarMenuItem>
+                    <Show when={user.isAdmin}>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton onClick={() => navigate("/statuses")}>
+                          Statuses
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                      <SidebarMenuItem>
+                        <SidebarMenuButton onClick={() => navigate("/users")}>
+                          Users
+                        </SidebarMenuButton>
+                      </SidebarMenuItem>
+                    </Show>
+                  </SidebarMenu>
+                </SidebarGroupContent>
+              </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton onClick={() => navigate("/settings")}>
+                      Settings
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                  <SidebarMenuItem>
+                    <SidebarMenuButton onClick={() => navigate(`/profile/${user.id}`)}>
+                      Profile
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarFooter>
+          </Sidebar>
+        </Show>
+        <main class="w-full h-full">
+          <Show when={user.username}>
+            <SidebarTrigger />
+          </Show>
+          {props.children}
+        </main>
+      </SidebarProvider>
+    )
+}
+export default AppSidebar
